7d24c7d60cb6f7010524118d953809dfa65fa1a3
Andre <dat.andre.on.juno@gmail.com>
2024-11-09 09:50:09 +0000

    add errors

--- programs/predicoid_1adaykeepsthedocteraway/src/error.rs
+++ programs/predicoid_1adaykeepsthedocteraway/src/error.rs
@@ -2,6 +2,9 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+    #[msg("Market name too long")]
+    MarketNameTooLong,
+
+    #[msg("Fee not allowed")]
+    FeeOutOfBounds,
 }
