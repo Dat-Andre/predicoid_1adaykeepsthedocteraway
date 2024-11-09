bd02d66bc3e1a29536eb4995e4798bb387646789
Andre <dat.andre.on.juno@gmail.com>
2024-11-09 12:45:21 +0000

    errors for initializations

--- programs/predicoid_1adaykeepsthedocteraway/src/error.rs
+++ programs/predicoid_1adaykeepsthedocteraway/src/error.rs
@@ -5,6 +5,12 @@ pub enum ErrorCode {
     #[msg("Market name too long")]
     MarketNameTooLong,
 
+    #[msg("Pool event name too long")]
+    EventNameTooLong,
+
+    #[msg("Pool side name too long")]
+    SideNameTooLong,
+
     #[msg("Fee not allowed")]
     FeeOutOfBounds,
 }
