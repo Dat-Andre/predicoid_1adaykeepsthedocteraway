c8b46c75d436126bbb118fae8ee4f18aa4c91555
Andre <dat.andre.on.juno@gmail.com>
2024-11-09 12:46:23 +0000

    state adjustments

--- programs/predicoid_1adaykeepsthedocteraway/src/instructions/mod.rs
+++ programs/predicoid_1adaykeepsthedocteraway/src/instructions/mod.rs
@@ -3,3 +3,6 @@ pub use initialize_platform::*;
 
 pub mod initialize_market;
 pub use initialize_market::*;
+
+pub mod initialize_pool;
+pub use initialize_pool::*;
--- programs/predicoid_1adaykeepsthedocteraway/src/state/pool_config.rs
+++ programs/predicoid_1adaykeepsthedocteraway/src/state/pool_config.rs
@@ -3,11 +3,15 @@ use anchor_lang::prelude::*;
 #[account]
 pub struct PoolConfig {
     pub market_admin: Pubkey,
-    pub pool_status: u8,
+    /* pub pool_status: u8, */
     /* pub pool_fee: u64, */
     pub pool_vault_state_bump: u8,
     pub bump: u8,
     pub event: String,
     pub side_a: String,
     pub side_b: String,
+}
+
+impl Space for PoolConfig {
+    const INIT_SPACE: usize = 8 + 32 + 1 + 1 + (4 + 32) + (4 + 16) + (4 + 16);
 }
